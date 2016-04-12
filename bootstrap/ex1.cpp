24bbf6cfda08190f3324f333d701c6ad1c6d81e8
Cloquet <alban.cloquet@epitech.eu>
2016-04-12 19:55:58 +0200

    PUSH

--- bootstrap/ex1.cpp
+++ bootstrap/ex1.cpp
@@ -11,12 +11,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "Mutex.hpp"
-#include "ScopedLock.hpp"
+#include "SafeQueue.hpp"
 
-typedef struct  count {
-    int         var;
-    Mutex       mutex;
-}               count;
+typedef struct  	count {
+    int			var;
+    Mutex		mutex;
+    SafeQueue<int>	queue;
+}               	count;
 
 void init(count *c)
 {
@@ -29,9 +30,24 @@ void *increment_counter(void *N)
 
     for (size_t i = 0; i < 1000000; i++)
     {
-      ScopedLock *scp = new ScopedLock(param->mutex);
+      param->mutex.lock();
       ++(param->var);
-      delete scp;
+	param->queue.push(param->var);
+      param->mutex.unlock();
+    }
+  return NULL;
+}
+
+void *decrement_counter(void *N)
+{
+  count *param = static_cast<count *>(N);
+
+  for (size_t i = 0; i < 1000000; i++)
+    {
+      param->mutex.lock();
+      ++(param->var);
+      param->queue.tryPop(&param->var);
+      param->mutex.unlock();
     }
   return NULL;
 }
@@ -45,10 +61,14 @@ int main()
   init(&c);
   printf("test avec mutex\n");
   pthread_create(&pt1, NULL, &increment_counter, &c);
-  pthread_create(&pt2, NULL, &increment_counter, &c);
+  pthread_create(&pt2, NULL, &decrement_counter, &c);
 
   pthread_join(pt1, NULL);
   pthread_join(pt2, NULL);
   printf("test avec mutex :[%i]\n", c.var);
+
+  c.queue.setFinished();
+  if (c.queue.isFinished() == true)
+    std::cout << "Ca marche bien" << std::endl;
   return 0;
 }
