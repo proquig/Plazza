4e7fb1eaa5482d08d86e159880bc7b4cb8fabe78
Guillaume Proquin <guillaume.proquin@epitech.eu>
2016-04-13 19:10:24 +0200

    start parsing order

--- src/OrderReader.cpp
+++ src/OrderReader.cpp
@@ -7,34 +7,43 @@
 #include <algorithm>
 #include "OrderReader.h"
 
-Plazza::OrderReader::OrderReader(std::queue<IOrder *> orders) : orders(orders)
+void				Plazza::OrderReader::OrderReader(std::queue<IOrder *> orders) : orders(orders)
 {
-  std::string line;
-  while (std::cin)
+  /*
+    std::string line;
+    while (std::cin)
     {
-      std::getline(std::cin, line);
-      parseLine(line);
+    std::getline(std::cin, line);
+    parseLine(line);
     }
+  */
 }
 
-void Plazza::OrderReader::parseCommand(const std::string &command)
+void				Plazza::OrderReader::parseCommand(const std::string &command)
 {
-  std::vector<std::string> files;
-  std::string type;
-  IOrder *order;
+  std::vector<std::string>	files;
+  std::stringstream		commandStream(command);
+  std::string			type;
+  IOrder			*order;
 
-  // TODO :create order
-
-  this->orders.push(order);
+  while (std::getline(commandStream, word, ' '))
+    if (word.length())
+      files.push_back(word);
+  order = files.back();
+  files.pop_back();
+  //this->orders.push(order);
 }
 
 void Plazza::OrderReader::parseLine(const std::string &line)
 {
-  std::vector<std::string> commands;
+  //std::vector<std::string> commands;
 
-  // TODO : create commands
+  //for (std::vector<std::string>::iterator it; it != commands.end(); it++)
+  //parseCommand(*it);
+  std::stringstream               lineStream(line);
+  std::string                     command;
 
-  for (std::vector<std::string>::iterator it; it != commands.end(); it++)
-    parseCommand(*it);
+  while (std::getline(lineStream, command, ';'))
+    if (command.length())
+      parseCommand(command);
 }
-
