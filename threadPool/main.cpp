bb50c30b5eaf704e1447f41753aed71fdae57c12
Cloquet <alban.cloquet@epitech.eu>
2016-04-18 20:11:01 +0200

    Ahah cette threadpOol de fout

---
+++ threadPool/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+#include "ThreadPool.h"
+
+int main()
+{
+// create thread pool with 4 worker threads
+ThreadPool pool(4);
+
+// enqueue and store future
+auto result = pool.enqueue([](int answer) { return answer; }, 42);
+
+// get result from future
+std::cout << result.get() << std::endl;
+return 0;
+}
