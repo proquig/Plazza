fb3dae721ac0229d0f7ca3e0841de8025fed5e81
Gauthier POGAM--LE MONTAGNER <gauthier.pogam-lemontagner@epitech.eu>
2016-04-18 21:01:16 +0200

    Use factory

--- src/OrderReader.cpp
+++ src/OrderReader.cpp
@@ -6,10 +6,15 @@
 #include <vector>
 #include <algorithm>
 #include <sstream>
-#include "OrderReader.h"
+#include "OrderReader.hpp"
+#include "orders/EmailAddress.hpp"
+#include "orders/PhoneNumber.hpp"
 
 Plazza::OrderReader::OrderReader()
 {
+  this->_factory.registerType("EMAIL_ADDRESS", new EmailAddress);
+  this->_factory.registerType("IP_ADDRESS", new EmailAddress);
+  this->_factory.registerType("PHONE_NUMBER", new PhoneNumber);
 }
 
 void				Plazza::OrderReader::parseCommand(const std::string &command)
@@ -24,6 +29,9 @@ void				Plazza::OrderReader::parseCommand(const std::string &command)
       files.push_back(word);
   orderType = files.back();
   files.pop_back();
+
+  for (std::vector<std::string>::iterator it = files.begin(); it != files.end(); it++)
+    this->_orders.push(this->_factory.create(orderType));
 }
 
 void Plazza::OrderReader::parseLine(const std::string &line)
@@ -34,9 +42,4 @@ void Plazza::OrderReader::parseLine(const std::string &line)
   while (std::getline(lineStream, command, ';'))
     if (command.length())
       parseCommand(command);
-}
-
-const std::vector<std::string> &Plazza::OrderReader::getCommands() const
-{
-  return commands;
 }
