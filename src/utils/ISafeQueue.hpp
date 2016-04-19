42492287bb5eb1cb46c46ee07633367e0f68f064
Gauthier POGAM--LE MONTAGNER <gauthier.pogam-lemontagner@epitech.eu>
2016-04-19 13:28:19 +0200

    Front/back functions

--- src/utils/ISafeQueue.hpp
+++ src/utils/ISafeQueue.hpp
@@ -1,18 +1,25 @@
 #ifndef CPP_PLAZZA_ISAFEQUEUE_HPP
 #define CPP_PLAZZA_ISAFEQUEUE_HPP
 
-template<typename T>
-class ISafeQueue
+namespace Plazza
 {
- public:
-  virtual ~ISafeQueue(void)
-  { }
+  template<typename T>
+  class ISafeQueue
+  {
+   public:
+    virtual ~ISafeQueue(void)
+    { }
 
-  virtual int pop(void) = 0;
+    virtual int pop(void) = 0;
 
-  virtual void push(const T value) = 0;
+    virtual void push(const T value) = 0;
 
-  virtual bool tryPop(T *value) = 0;
-};
+    virtual bool tryPop(T *value) = 0;
+
+    virtual T &front() = 0;
+
+    virtual T &back() = 0;
+  };
+}
 
 #endif
